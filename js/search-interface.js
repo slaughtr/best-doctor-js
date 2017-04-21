b5c4654b6490c4ac3874c52fdee8c8a90c91ba8b
slaughtr <dslaughtr@gmail.com>
2017-04-21 13:25:30 -0700

    Add basic information from API call to table

--- js/search-interface.js
+++ js/search-interface.js
@@ -3,6 +3,31 @@ const getDoctors = require('./../js/search.js').getDoctors
 $(function () {
   $('#searchCondition').click(function () {
     let medicalIssue = $('#inputCondition').val()
-    console.log(getDoctors(medicalIssue))
-})
+
+    getDoctors(medicalIssue).done((result) => {
+      let doctor = result.data
+      doctor.forEach(function (doctor) {
+        $('#showResults').append('<tr>')
+
+        $('#showResults').append('<td> <img src="' + doctor.profile.image_url + '"> </td>')
+        $('#showResults').append('<td>' + doctor.profile.first_name + ' ' + doctor.profile.last_name + ', ' + doctor.profile.title + '</td>')
+        // $('#showResults').append('<td>')
+        doctor.practices.forEach(function (practice) {
+          $('#showResults').append(practice.name + '<br>')
+        })
+
+        if (doctor.practices[0].accepts_new_patients === true)
+        {
+          $('#showResults').append('<td> Yes </td>')
+        } else {
+          $('#showResults').append('<td> No </td>')
+        }
+        // $('#showResults').append('</td>')
+        $('#showResults').append('</tr>')
+
+      })
+
+    })
+
+  })
 })
--- js/search.js
+++ js/search.js
@@ -1,9 +1,10 @@
 const apiKey = require('./../.env').apiKey
 
 exports.getDoctors = function (medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  return $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function (result) {
-    console.log(result)
+    // console.log(result.data[0].profile.first_name)
+    return result
   })
   .fail(function (error) {
     console.log('fail, : ' + error)
