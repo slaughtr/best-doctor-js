c416f3bb4dc4bce046b786fc5ac6aa6b6957a85e
slaughtr <dslaughtr@gmail.com>
2017-04-21 16:58:55 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,30 @@
+# _{Best Doctor}_
+
+#### _{Using the BetterDoctor API to search for doctors in the Portland area}, {4-21-2017}_
+
+#### By _**{Dallas Slaughter}**_
+
+## Description
+
+_{This is a page where you can enter }_
+
+## Setup/Installation Requirements
+
+* _Clone this repo to your local computer_
+* _CD into the directory in your terminal_
+* _Type 'npm init && bower init'_
+* _After init has finished, type 'gulp serve_
+
+## Support and contact details
+
+_{If you have any questions/concerns, email me at dslaughtr@gmail.com or message me here on GitHub}_
+
+## Technologies Used
+
+_{JavaScript, HTML, CSS, jQuery, SASS}_
+
+### License
+
+*{Licensed under the MIT license.}*
+
+Copyright (c) 2017 **_{Dallas Slaughter}_**
--- js/search-interface.js
+++ js/search-interface.js
@@ -43,7 +43,7 @@ $(function () {
 
         $('#showResults').append('<tr>')
         //most of the image properties below are for the popover
-        $('#showResults').append('<td> <img id="imageId" data-container="body" data-trigger="manual" data-toggle="popover" title="' + doctor.profile.first_name + '" data-content="' + doctor.profile.bio + '"  data-options=\'{ "image" : "' + doctor.profile.image_url + '"}\' src="' + doctor.profile.image_url + '"> </td>')
+        $('#showResults').append('<td> <img id="imageId" data-container="body" data-trigger="manual" data-toggle="popover" title="' + doctor.profile.first_name + '" data-content="<p>' + doctor.profile.bio + '</p>"  data-html="true" data-options=\'{ "image" : "' + doctor.profile.image_url + '"}\' src="' + doctor.profile.image_url + '"> </td>')
         $('#showResults').append('<td>' + doctor.profile.first_name + ' ' + doctor.profile.last_name + ', ' + doctor.profile.title + '</td>')
 
         doctor.practices.forEach(function (practice, i) {
